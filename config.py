adf23ebe8e4b49748b21907cbec9ffd2e9a367e2
shwpat <shwetadpatil@yahoo.com>
2026-08-24 18:08:49 -0400

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,9 @@ from langchain.tools import tool
 from langchain_deepseek import ChatDeepSeek
 
 load_dotenv()
+data_path = path(r".....")
+tickets_file = data_path / "tickets.csv"
+policies_file = data_path / "policies.csv"
 
 llm = ChatDeepSeek(
     model="deepseek-v4-flash",
